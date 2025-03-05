5a85740f6103adf2cce3bf880b3cded7b7f0c156
aLanois14 <axel.lanois@outlook.fr>
2025-03-05 15:34:40 +0100

    all test pass + cover all

--- gilded-rose.spec.ts
+++ gilded-rose.spec.ts
@@ -5,7 +5,7 @@ describe("Gilded Rose", () => {
   it("should foo", () => {
     const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
 
   it("should Aged Brie", () => {
