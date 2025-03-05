ef0fa145ac81436f0cecb022d3f23f6a274e181d
aLanois14 <axel.lanois@outlook.fr>
2025-03-05 16:05:17 +0100

    complete with more tests

--- gilded-rose.spec.ts
+++ gilded-rose.spec.ts
@@ -1,38 +1,88 @@
 import { describe, expect } from "@jest/globals";
-import { GildedRose, Item } from "./gilded-rose";
+import {
+  AGEDBRIE,
+  BACKSTAGEPASSES,
+  GildedRose,
+  Item,
+  SULFURAS,
+} from "./gilded-rose";
 
 describe("Gilded Rose", () => {
-  it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+  it("doit diminuer la qualité d'un article normal", () => {
+    const gildedRose = new GildedRose([new Item("foo", 10, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(items[0].quality).toBe(19);
   });
 
-  it("should Aged Brie", () => {
-    const gildedRose = new GildedRose([new Item("Aged Brie", 0, 0)]);
+  it("doit diminuer sellIn d'un article normal", () => {
+    const gildedRose = new GildedRose([new Item("foo", 10, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("Aged Brie");
+    expect(items[0].sellIn).toBe(9);
   });
 
-  it("should Backstage passes to a TAFKAL80ETC concert", () => {
-    const gildedRose = new GildedRose([
-      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0),
-    ]);
+  it("doit diminuer la qualité deux fois plus vite après expiration", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].quality).toBe(8);
   });
 
-  it("should Sulfuras, Hand of Ragnaros", () => {
-    const gildedRose = new GildedRose([
-      new Item("Sulfuras, Hand of Ragnaros", 0, 0),
-    ]);
+  it("ne doit jamais avoir une qualité inférieure à 0", () => {
+    const gildedRose = new GildedRose([new Item("foo", 5, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[0].quality).toBe(0);
   });
 
-  it("should foo 50", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 50)]);
+  it("doit augmenter la qualité de Aged Brie", () => {
+    const gildedRose = new GildedRose([new Item(AGEDBRIE, 10, 10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(items[0].quality).toBe(11);
+  });
+
+  it("ne doit pas augmenter la qualité de Aged Brie au-dessus de 50", () => {
+    const gildedRose = new GildedRose([new Item(AGEDBRIE, 10, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("doit augmenter la qualité des Backstage passes si sellIn > 10", () => {
+    const gildedRose = new GildedRose([new Item(BACKSTAGEPASSES, 11, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(21);
+  });
+
+  it("doit augmenter la qualité des Backstage passes de 2 si sellIn est 10", () => {
+    const gildedRose = new GildedRose([new Item(BACKSTAGEPASSES, 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(22);
+  });
+
+  it("doit augmenter la qualité des Backstage passes de 3 si sellIn est 5", () => {
+    const gildedRose = new GildedRose([new Item(BACKSTAGEPASSES, 5, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(23);
+  });
+
+  it("doit réduire la qualité des Backstage passes à 0 après expiration", () => {
+    const gildedRose = new GildedRose([new Item(BACKSTAGEPASSES, 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("ne doit pas diminuer la qualité de Sulfuras", () => {
+    const gildedRose = new GildedRose([new Item(SULFURAS, 10, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+
+  it("ne doit pas diminuer sellIn de Sulfuras", () => {
+    const gildedRose = new GildedRose([new Item(SULFURAS, 10, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(10);
+  });
+
+  it("doit augmenter la qualité de Aged Brie après expiration", () => {
+    const gildedRose = new GildedRose([new Item(AGEDBRIE, -1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(12);
   });
 });
--- gilded-rose.ts
+++ gilded-rose.ts
@@ -20,20 +20,18 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+        this.items[i].name != AGEDBRIE &&
+        this.items[i].name != BACKSTAGEPASSES
       ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].name != SULFURAS) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (this.items[i].name == BACKSTAGEPASSES) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -47,16 +45,14 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (this.items[i].name != SULFURAS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+        if (this.items[i].name != AGEDBRIE) {
+          if (this.items[i].name != BACKSTAGEPASSES) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].name != SULFURAS) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
@@ -75,3 +71,7 @@ export class GildedRose {
     return this.items;
   }
 }
+
+export const AGEDBRIE = "Aged Brie";
+export const BACKSTAGEPASSES = "Backstage passes to a TAFKAL80ETC concert";
+export const SULFURAS = "Sulfuras, Hand of Ragnaros";
